bca1ad88796b5b3e50674ca2d26ac09fd00f9874
Lukaa98 <niqabadzeluka@gmail.com>
2023-05-13 13:55:31 -0400

    using hashrouter instead of browser router as that might be reason why application is failing during reload when depolyed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import MyHome from './pages/MyHome'
 import AboutMe from './pages/AboutMe'
 import Header from './Header'
