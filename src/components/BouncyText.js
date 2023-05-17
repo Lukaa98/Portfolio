27d85769979a693b9b4237ed9b6139b55758b7ab
Lukaa98 <niqabadzeluka@gmail.com>
2023-05-17 17:52:41 -0400

    in this commit i have updated function of bouncetext for smoother animation, removed background for header, updated theme for consistant fonts and typography.

--- src/components/BouncyText.js
+++ src/components/BouncyText.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
 
-const BouncyText = ({ text }) => {
-  const BouncyTextStyled = styled(Typography)(({ theme }) => ({ ...theme.bouncyText }));
+const BouncyText = ({ text, fontFamily, color, fontSize }) => {
+  const BouncyTextStyled = styled(Typography)(({ theme }) => ({
+    ...theme.bouncyText,
+    fontFamily: fontFamily || theme.bouncyText.fontFamily,
+  }));
+
+  const SpanStyled = styled('span')(({ theme }) => ({
+    fontSize: fontSize || theme.bouncyText.fontSize,
+    color: color || theme.bouncyText.color,
+  }));
 
   return (
     <BouncyTextStyled>
-      {text.split('').map((char, i) => (
-        <span key={`bouncy-char-${i}`}>{char}</span>
+      {text.split(' ').map((word, i) => (
+        <React.Fragment key={`bouncy-word-${i}`}>
+          {i !== 0 && ' '}
+          {word.split('').map((char, j) => (
+            <SpanStyled key={`bouncy-char-${i}-${j}`}>{char}</SpanStyled>
+          ))}
+        </React.Fragment>
       ))}
     </BouncyTextStyled>
   );
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,6 @@
 export const darkTheme = {
   palette: {
+    mode: 'dark', // Add this line
     primary: {
       main: '#2196f3',
     },
@@ -7,10 +8,17 @@ export const darkTheme = {
       main: '#f50057',
     },
     background: {
-      default: '#121212',
+      default: '#03141D',
     },
     text: {
-      primary: '#ffffff',
+      primary: '#97BCC7',
+    },
+  },
+  typography: {
+    fontFamily: 'Share Tech Mono, Arial',
+    fontSize: '100rem',
+    h1: {
+      fontFamily: '"Share Tech Mono", "Arial", sans-serif',
     },
   },
   mouseCircle: {
@@ -18,30 +26,38 @@ export const darkTheme = {
     height: '1000px',
     borderRadius: '50%',
     backgroundColor: 'transparent',
-    backgroundImage: 'radial-gradient(circle at center, #28484B, #000000, #000000)', // Add an extra color stop to make the transition smoother
+    backgroundImage: 'radial-gradient(circle at center, rgba(6, 40, 58, 0.2), rgba(3, 20, 29, 0.2), rgba(3, 20, 29, 0.2))',
     pointerEvents: 'none',
     zIndex: -1,
-    animation: 'pulse 2s infinite', // Add pulsing animation
+    animation: 'pulse 2s infinite',
   },
+
   bouncyText: {
     display: 'inline',
     fontSize: '5rem',
     '& span': {
       display: 'inline-block',
-      transition: '0.2s',
+      transition: '0.3s',
       '&:hover': {
-        transform: 'translateY(-10px)',
-        color: 'blue',
+        transform: 'translateY(-20px)',
+        color: '#053D57',
       },
       '&:nth-child(odd):hover': {
-        transform: 'translateY(-15px)',
+        transform: 'translateY(-25px)',
       },
     },
+  },
+  shareTechMonoFont: {
+    fontFamily: 'Share Tech Mono, monospace',
+  },
+  chonburiFont: {
+    fontFamily: 'Chonburi, cursive',
   },
 }
 
 export const lightTheme = {
   palette: {
+    mode: 'light', // Add this line
     primary: {
       main: '#2196f3',
     },
@@ -49,10 +65,17 @@ export const lightTheme = {
       main: '#f50057',
     },
     background: {
-      default: '#f5f5f5',
+      default: '#c3e0e8',
     },
     text: {
-      primary: '#333333',
+      primary: '#006884',
+    },
+  },
+  typography: {
+    fontFamily: 'Share Tech Mono',
+    h1: {
+      fontFamily: 'Share Tech Mono',
+      
     },
   },
   mouseCircle: {
@@ -60,24 +83,31 @@ export const lightTheme = {
     height: '1000px',
     borderRadius: '50%',
     backgroundColor: 'transparent',
-    backgroundImage: 'radial-gradient(circle at center, #D9FCFF, #f5f5f5, #f5f5f5)', // Add an extra color stop to make the transition smoother
+    backgroundImage: 'radial-gradient(circle at center, rgba(87, 136, 151, 0.2), rgba(195, 224, 232, 0.2), rgba(195, 224, 232, 0.2))',
     pointerEvents: 'none',
     zIndex: -1,
-    animation: 'pulse 2s infinite', // Add pulsing animation
+    animation: 'pulse 2s infinite',
   },
+
   bouncyText: {
     display: 'inline',
     fontSize: '5rem',
     '& span': {
       display: 'inline-block',
-      transition: '0.2s',
+      transition: '0.3s',
       '&:hover': {
-        transform: 'translateY(-10px)',
-        color: 'blue',
+        transform: 'translateY(-20px)',
+        color: 'white',
       },
       '&:nth-child(odd):hover': {
-        transform: 'translateY(-15px)',
+        transform: 'translateY(-25px)',
       },
     },
+  },
+  shareTechMonoFont: {
+    fontFamily: 'Share Tech Mono, monospace',
+  },
+  chonburiFont: {
+    fontFamily: 'Chonburi, cursive',
   },
 }
