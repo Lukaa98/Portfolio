a2ebe55310810c303d9dc53d2d5ce96cccc96d0d
Lukaa98 <niqabadzeluka@gmail.com>
2023-05-30 12:50:48 -0400

    reverted back to previus code before pushing

--- src/pages/MySkills.js
+++ src/pages/MySkills.js
@@ -5,42 +5,37 @@ import Grid from '@material-ui/core/Grid'
 import BouncyText from '../components/BouncyText'
 import MyTagCloud from '../components/TagCloud'
 import { useTheme } from '@mui/material/styles'
-import useMediaQuery from '@mui/material/useMediaQuery'
 
 function MySkills() {
   const theme = useTheme()
   const isLightTheme = theme.palette.mode === 'light'
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'))
-
+  
   return (
-    <Grid container 
-          style={{ marginTop: '50px' }} 
-          direction={isSmallScreen ? 'column' : 'row'} 
-          justifyContent="center" 
-          alignItems="center"
-          spacing={2}
-    >
-      <Grid item xs={12} md={6}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems:'center', textAlign: 'center' }}>
-          <BouncyText
-            text="My Skills!"
-            fontFamily={theme.bouncyText.fontFamily}
-            color={isLightTheme ? '#053D57' : 'white'}
-            fontSize={isSmallScreen ? "60px" : "110px"}
+    <>
+      <Grid container style={{ marginTop: '50px' }}>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems:'left', marginLeft:'100px' }}>
+            <BouncyText
+              text="My Skills!"
+              fontFamily={theme.bouncyText.fontFamily}
+              color={isLightTheme ? '#053D57' : 'white'}
+              fontSize="110px"
+              />
+            <MyTagCloud />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '40px' }}>
+            <iframe
+              src={process.env.PUBLIC_URL + '/ResumeLukaNikabadze.pdf'}
+              frameBorder={0}
+              width="702px"
+              height="894px"
             />
-          <MyTagCloud />
-        </Box>
+          </Box>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <iframe
-            src={process.env.PUBLIC_URL + '/ResumeLukaNikabadze.pdf'}
-            frameBorder={0}
-            style={{width: '50vw', height: '70vh'}}
-          />
-        </Box>
-      </Grid>
-    </Grid>
+    </>
   )
 }
 
